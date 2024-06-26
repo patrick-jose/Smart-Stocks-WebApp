299279bf2219ab82572a9ce11ef8e60d8547a180
patrick-jose <patric-jose@hotmail.com>
2024-06-26 19:40:13 -0300

    #4 Minor adjustments

--- src/app/layout/menu-bar/menu-bar.component.ts
+++ src/app/layout/menu-bar/menu-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -9,7 +9,7 @@ import {MatMenuModule} from '@angular/material/menu';
 @Component({
   selector: 'menu-bar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
